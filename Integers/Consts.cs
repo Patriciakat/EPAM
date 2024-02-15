27ef6d4da012090d275c867049216e1496c6b60b
Patricija Katinaite <116752299+Patriciakat@users.noreply.github.com>
2024-02-16 01:47:56 +0200

    Add files via upload

---
+++ Integers/Consts.cs
@@ -0,0 +1,65 @@
+namespace Literals
+{
+    public static class Consts
+    {
+        public static int ReturnLargestInteger()
+        {
+            // TODO #3-1. Return the largest possible value of the int type.
+            return 2_147_483_647;
+        }
+
+        public static int ReturnSmallestInteger()
+        {
+            // TODO #3-2. Return the smallest possible value of the int type.
+            return -2147483648;
+        }
+
+        public static uint ReturnLargestUnsignedInteger()
+        {
+            // TODO #3-3. Return the largest possible value of the uint type.
+            return 4294967295;
+        }
+
+        public static uint ReturnSmallestUnsignedInteger()
+        {
+            // TODO #3-4. Return the smallest possible value of the uint type.
+            return 0;
+        }
+
+        public static long ReturnLargestLongInteger()
+        {
+            // TODO #3-5. Return the largest possible value of the long type.
+            return 9223372036854775807;
+        }
+
+        public static long ReturnSmallestLongInteger()
+        {
+            // TODO #3-6. Return the smallest possible value of the long type.
+            return -9223372036854775808;
+        }
+
+        public static ulong ReturnLargestUnsignedLongInteger()
+        {
+            // TODO #3-7. Return the largest possible value of the ulong type.
+            return 18446744073709551615;
+        }
+
+        public static ulong ReturnSmallestUnsignedLongInteger()
+        {
+            // TODO #3-8. Return the smallest possible value of the ulong type.
+            return 0;
+        }
+
+        public static short ReturnSmallestShortInteger()
+        {
+            // TODO #3-9. Return the smallest possible value of the short type.
+            return -32768;
+        }
+
+        public static short ReturnLargestShortInteger()
+        {
+            // TODO #3-10. Return the largest possible value of the short type.
+            return 32767;
+        }
+    }
+}
